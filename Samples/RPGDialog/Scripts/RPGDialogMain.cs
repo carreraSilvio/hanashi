19a4adbd6ade7311cea6a1e6dfa40aaa169fb43f
Silvio Carrera <carrera.silvio@gmail.com>
2021-07-17 16:14:23 -0300

    WIP RPGDialogSample

---
+++ Samples/RPGDialog/Scripts/RPGDialogMain.cs
@@ -0,0 +1,34 @@
+﻿using Hanashi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+namespace HanashiSamples
+{
+    public class RPGDialogMain : MonoBehaviour
+    {
+        public NarrativeData narrativeData;
+
+        public Text speaker;
+        public Text message;
+        private int _currentNode;
+        private int _totalNodes;
+
+        void Awake()
+        {
+            speaker.text = "";
+            message.text = "";
+            _totalNodes = narrativeData.Nodes.Count;
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                
+            }
+        }
+
+    }
+}
